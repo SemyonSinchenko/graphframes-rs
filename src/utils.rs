ed2825d1058636c6deb05b0bb6244b439f7b7c68
Sem <ssinchenko@apache.org>
2026-07-06 14:55:42 +0200

    feat: make tests green again (#29)
    * feat: make tests green again
    
    * fix: null semantic differs?
    
    * fix: null semantic
    
    looks like 0 on my machine and 1 inside gh runners
    
    * fix: final fix?

--- src/utils.rs
+++ src/utils.rs
@@ -6,4 +6,4 @@ pub use options::GraphFramesConfig;
 pub(crate) use options::scoped_ctx;
 
 #[cfg(test)]
-pub(crate) use testing_utils::{assert_dataframes_equal, create_ldbc_test_graph};
+pub(crate) use testing_utils::{assert_dataframes_equal, collect_to_i64, create_ldbc_test_graph};
